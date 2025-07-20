b40c57585dcfcee55b146f65668964672f8f0300
Nestor Toscano <toscanonestor07@gmail.com>
2025-07-20 15:07:42 -0500

    update

---
+++ script_testing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# make sure to add execution permission 700 / +x
+# or source / bash the file without setting permissions
+
+# Loading env file
+source .env
+
+echo "Username is $USERNAME"
+echo "API key is $API_KEY"
