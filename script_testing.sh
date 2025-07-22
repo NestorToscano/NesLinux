546a3360561a5ce36fbffc8fa979b9d144ad7585
Nestor Toscano <toscanonestor07@gmail.com>
2025-07-22 11:48:58 -0500

    added more notes

--- script_testing.sh
+++ script_testing.sh
@@ -5,5 +5,23 @@
 # Loading env file
 source .env
 
-echo "Username is $USERNAME"
-echo "API key is $API_KEY"
+read -p "What var from the .env file would you like? " first_var
+read -p "Whats another var from the .env file would you like? " second_var
+
+#  -n "${!first_var}" : indirect expansion check if has a value (checks ref var)
+# -n "$first_var" : is just checking for input
+#  -v "$first_var" : check if variable exists at all (checks name)
+
+if [ -v "$first_var" ]
+then 
+	echo "$first_var is ${!first_var}"
+else
+	echo "$first_var does not exist"
+fi
+
+if [ -v "$second_var" ]
+then
+	echo "$second_var is ${!second_var}"
+else 
+	echo "$second_var does not exist"
+fi
